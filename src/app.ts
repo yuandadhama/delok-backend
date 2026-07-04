7981aa001090a86eb00ec8f1d94bcf4e5aac7ae1
yuandadhama <yuandadhamap@gmail.com>
2026-07-04 13:03:34 +0700

    feat: add cors middleware to express app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // app.ts
 import express from "express";
+import cors from "cors";
 
 import { userRoute } from "./modules/user/user.route";
 import { errorMiddleWare } from "./middlewares/error.middleware";
@@ -7,6 +8,16 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 
 export const app = express();
+
+//configure cors middleware
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 // auth route better auth setting
 app.all("/api/auth/*splat", toNodeHandler(auth));
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,6 +13,7 @@ if (!googleClientId || !googleClientSecret) {
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: ["http://localhost:3000"],
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
