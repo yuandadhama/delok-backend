12b08d36a3a63e774be6ca383b2f06c627eb98dd
yuandadhama <yuandadhamap@gmail.com>
2026-07-07 20:43:48 +0700

    feat: email verification backend added
    - set the callback url to point verified page after email verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,6 +19,8 @@ if (!githubClientId || !githubClientSecret) {
   throw new Error("Github OAuth env missing");
 }
 
+console.log("AUTH CONFIG LOADED");
+
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   trustedOrigins: ["http://localhost:3000"],
@@ -28,6 +30,7 @@ export const auth = betterAuth({
 
   emailAndPassword: {
     enabled: true,
+    requireEmailVerification: true,
 
     // reset password setup
     sendResetPassword: async ({ user, url }) => {
@@ -52,23 +55,29 @@ export const auth = betterAuth({
   },
 
   emailVerification: {
-    sendOnSignIn: true,
-
     // verification email setup
     sendVerificationEmail: async ({ user, url }) => {
+      const verifyUrl = new URL(url);
+
+      verifyUrl.searchParams.set(
+        "callbackURL",
+        "http://localhost:3000/sign-up/verified",
+      );
+
       await resend.emails.send({
         from: "Delok <onboarding@resend.dev>",
         to: user.email,
         subject: "Verify your email",
         html: /* html */ `
         <div>  
-          <h1>Verify your email</h1>
-          <p>Hi ${user.name},</p>
-          <p>Click <a href="${url}">here</a> to verify your email.</p>
+        <h1>Verify your email</h1>
+        <p>Hi ${user.name},</p>
+        <p>Click <a href="${verifyUrl.toString()}">here</a> to verify your email.</p>
         </div>
-          `,
+        `,
       });
     },
+    sendOnSignIn: true,
   },
 
   socialProviders: {
