698d5e2cba09e6c2965c497a9d2c67ef6b7c0fa6
yuandadhama <yuandadhamap@gmail.com>
2026-08-03 14:01:38 +0700

    refactor(user): drop unused CRUD layers, keep session `/me` endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,14 +1,6 @@
 // /src/modules/user/user.controller.ts
 
 import { Request, Response } from "express";
-import {
-  createUserService,
-  deleteUserService,
-  getAllUserService,
-  getUserByIdService,
-  searchUserByNameService,
-  updateUserService,
-} from "../user/user.service";
 
 /**
  * GET /api/user/me
@@ -18,94 +10,5 @@ export const meController = async (req: Request, res: Response) => {
   res.json({
     success: true,
     data: req.session,
-  });
-};
-
-/**
- * GET /api/user
- * Get all users
- */
-export const getAllUserController = async (req: Request, res: Response) => {
-  const data = await getAllUserService();
-  res.json({
-    success: true,
-    data,
-  });
-};
-
-/**
- * GET /api/user/search?name=<keyword>
- * Search users by name
- */
-export const searchUserByNameController = async (
-  req: Request,
-  res: Response,
-) => {
-  const name = String(req.query.name);
-  const data = await searchUserByNameService(name);
-  res.json({
-    success: true,
-    data,
-  });
-};
-
-/**
- * GET /api/user/:id
- * Get single user by id
- */
-export const getUserByIdController = async (req: Request, res: Response) => {
-  const id = String(req.params.id);
-  const data = await getUserByIdService(id);
-  res.json({
-    success: true,
-    data,
-  });
-};
-
-/**
- * POST /api/user
- * Create new user
- */
-export const createUserController = async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-  const user = {
-    name,
-    email,
-  };
-  const data = await createUserService(user);
-  res.json({
-    success: true,
-    data,
-  });
-};
-
-/**
- * PUT /api/user/:id
- * Update existing user
- */
-export const updateUserController = async (req: Request, res: Response) => {
-  const id = String(req.params.id);
-  const { name, email } = req.body;
-  const user = {
-    name,
-    email,
-  };
-  const data = await updateUserService(id, user);
-  res.json({
-    success: true,
-    data,
-  });
-};
-
-/**
- * DELETE /api/user/:id
- * Delete user by id
- */
-export const deleteUserController = async (req: Request, res: Response) => {
-  const id = String(req.params.id);
-  const data = await deleteUserService(id);
-  res.json({
-    success: true,
-    data,
   });
 };
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,19 +1,9 @@
 // /src/modules/user/user.route.ts
 
 import express from "express";
-import {
-  createUserController,
-  deleteUserController,
-  getAllUserController,
-  getUserByIdController,
-  meController,
-  searchUserByNameController,
-  updateUserController,
-} from "./user.controller";
+import { meController } from "./user.controller";
 
-import { createUserSchema, updateUserSchema } from "./user.validation";
 import { asyncHandler } from "../../utils/async-handler";
-import { validate } from "../../middlewares/validate.middleware";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 
 export const userRoute = express.Router();
@@ -24,49 +14,6 @@ export const userRoute = express.Router();
 /**
  * GET /api/user/me
  * Get current user session
+ * this route is specially to check if the user is logged in and get the current user session
  */
 userRoute.get("/me", authMiddleware, asyncHandler(meController));
-
-/**
- * GET /api/user/search?name=<keyword>
- * Search users by name
- */
-userRoute.get("/search", asyncHandler(searchUserByNameController));
-
-/**
- * GET /api/user/:id
- * Get single user
- */
-userRoute.get("/:id", asyncHandler(getUserByIdController));
-
-/**
- * PUT /api/user/:id
- * Update existing user
- */
-userRoute.put(
-  "/:id",
-  validate(updateUserSchema),
-  asyncHandler(updateUserController),
-);
-
-/**
- * DELETE /api/user/:id
- * Delete user
- */
-userRoute.delete("/:id", asyncHandler(deleteUserController));
-
-/**
- * GET /api/user
- * Get all users
- */
-userRoute.get("/", asyncHandler(getAllUserController));
-
-/**
- * POST /api/user
- * Create new user
- */
-userRoute.post(
-  "/",
-  validate(createUserSchema),
-  asyncHandler(createUserController),
-);
